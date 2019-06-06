880b26ef954c23dc5ac8425c6d2c1e77327492c4
TheNickSkater <beinik6@gmx.at>
2019-06-06 14:26:42 +0200

    Cleanup, Fixes and safety checks

--- lua/entities/ttt_bear_trap/init.lua
+++ lua/entities/ttt_bear_trap/init.lua
@@ -32,54 +32,68 @@ local function DoBleed(ent)
 end
 
 function ENT:Touch(toucher)
-	if !IsValid(toucher) and !toucher:IsPlayer() then return end
+	if not IsValid(toucher) or not IsValid(self) then return end
 	if self:GetSequence() ~= 0 and self:GetSequence() ~= 2 then
 		self:SetPlaybackRate(1)
 		self:SetCycle(0)
 		self:SetSequence("Snap")
 		self:EmitSound("beartrap.wav")
+
+		if not toucher:IsPlayer() then
+			timer.Simple(0.1, function()
+				if not IsValid(self) then return end
+				self:SetSequence("ClosedIdle")
+			end)
+			return
+		end
+
 		toucher.IsTrapped = true
 		local dmg = DamageInfo()
 		dmg:SetAttacker(self.Owner)
 		dmg:SetInflictor(self)
 		dmg:SetDamage(8)
 		dmg:SetDamageType(DMG_GENERIC)
-		if toucher:IsPlayer() then
-			if TTT2 then -- add element to HUD if TTT2 is loaded
-				STATUS:AddStatus(toucher, "ttt2_beartrap")
-			end
 
-			toucher:ChatPrint("BearTrap: Dont't be sad. You have a little chance of escaping this trap :)")
-			timer.Create("beartrapdmg" .. toucher:EntIndex(), 1, 0, function()
-				if !IsValid(toucher) then timer.Destroy("beartrapdmg" .. toucher:EntIndex()) return end
-
-				local randint = math.random(1, 100)
-				if randint == 32 or randint == 76 or randint == 15 then
-					timer.Destroy("beartrapdmg" .. toucher:EntIndex())
-					toucher.IsTrapped = false
-					toucher:Freeze(false)
-					toucher:ChatPrint("BearTrap: You had luck and escaped the beartrap!")
-					
-					if TTT2 then -- remove element to HUD if TTT2 is loaded
-						STATUS:RemoveStatus(toucher, "ttt2_beartrap")
-					end
-
-					return
-				end
-
-				toucher:TakeDamageInfo(dmg)
-				toucher:Freeze(true)
-				DoBleed(toucher)
-				if !toucher:Alive() or !toucher.IsTrapped or !IsValid(self) then
-					timer.Destroy("beartrapdmg" .. toucher:EntIndex())
-					toucher.IsTrapped = false
-					toucher:Freeze(false)
-					return
-				end
-			end)
+		if TTT2 then -- add element to HUD if TTT2 is loaded
+			STATUS:AddStatus(toucher, "ttt2_beartrap")
 		end
 
-		timer.Simple(0.1, function() self:SetSequence("ClosedIdle") end)
+		toucher:ChatPrint("BearTrap: Dont't be sad. You have a little chance of escaping this trap :)")
+		timer.Create("beartrapdmg" .. toucher:EntIndex(), 1, 0, function()
+			if !IsValid(toucher) then timer.Destroy("beartrapdmg" .. toucher:EntIndex()) return end
+
+			local randint = math.random(1, 100)
+			if randint == 32 or randint == 76 or randint == 15 then
+				timer.Destroy("beartrapdmg" .. toucher:EntIndex())
+				toucher.IsTrapped = false
+				toucher:Freeze(false)
+				toucher:ChatPrint("BearTrap: You had luck and escaped the beartrap!")
+
+				if TTT2 then -- remove element to HUD if TTT2 is loaded
+					STATUS:RemoveStatus(toucher, "ttt2_beartrap")
+				end
+
+				return
+			end
+
+			toucher:TakeDamageInfo(dmg)
+			toucher:Freeze(true)
+			DoBleed(toucher)
+			if !toucher:Alive() or !toucher.IsTrapped or !IsValid(self) then
+				timer.Destroy("beartrapdmg" .. toucher:EntIndex())
+				toucher.IsTrapped = false
+				toucher:Freeze(false)
+				if TTT2 then -- remove element to HUD if TTT2 is loaded
+					STATUS:RemoveStatus(toucher, "ttt2_beartrap")
+				end
+				return
+			end
+		end)
+
+		timer.Simple(0.1, function()
+			if not IsValid(self) then return end
+			self:SetSequence("ClosedIdle")
+		end)
 	end
 end
 
@@ -94,7 +108,7 @@ if SERVER then
 end
 
 function ENT:Use(act)
-	if IsValid(act) and act:IsPlayer() then
+	if IsValid(act) and act:IsPlayer() and IsValid(self) then
 
 		if self.Owner:Alive() and act ~= self.Owner then
 			return
@@ -108,6 +122,7 @@ function ENT:Use(act)
 end
 
 function ENT:OnTakeDamage(dmg)
+	if not IsValid(self) then return end
 	self.dmg = self.dmg + dmg:GetDamage()
 	if self.dmg >= 25 then
 		if self:GetSequence() ~= 0 and self:GetSequence() ~= 2 then
